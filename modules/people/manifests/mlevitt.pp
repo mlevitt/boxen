8991660e2b4de973118fd29d2f3a2a4cdfed6585
Mark Levitt <mark.levitt@comcast.net>
2017-07-16 08:40:25 -0600

    Reconcile and restart

---
+++ modules/people/manifests/mlevitt.pp
@@ -0,0 +1,80 @@
+class people::mlevitt {
+  $home     = "/Users/${::boxen_user}"
+  $dotfiles = "${home}/.dotfiles"
+
+  repository { "${dotfiles}":
+    source  => 'mlevitt/dotfiles'
+  }
+
+  include chrome
+  include dropbox
+  include handbrake
+
+  include iterm2::stable
+  include iterm2::colors::solarized_light
+  include iterm2::colors::solarized_dark
+
+  include java
+  include kubectl
+  include ohmyzsh
+  include xquartz
+
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::disable_remote_control_ir_receiver
+  include osx::global::tap_to_click
+  include osx::finder::show_all_on_desktop
+  include osx::finder::no_file_extension_warnings
+  include osx::no_network_dsstores
+  class { 'osx::global::key_repeat_delay':
+    delay => 25
+  }
+  class { 'osx::global::key_repeat_rate':
+    rate => 2
+  }
+  osx::recovery_message {
+    'If this Mac is found, please call 801-792-2695':
+  }
+
+  include spectacle
+  include tmux
+  include vagrant
+  include virtualbox
+  include zsh
+
+package { 'Geektool':
+  source   => "https://dl.devmate.com/org.tynsoe.GeekTool/331.014/1470733752/GeekTool-331.014.zip",
+           provider => 'compressed_app',
+}
+
+  # Various homebrew packages
+  package { [
+    'dos2unix',
+    'eigen',
+    'gpg',
+    'jq',
+    'maven',
+    'pstree',
+    'pv',
+    'tree',
+    'watch',
+    ]:
+    ensure => 'present'
+  }
+
+  # Caskroom packages
+  package { [
+      'docker-edge',
+      'quicksilver',
+    ]:
+    ensure => present,
+    provider => 'brewcask',
+  }
+
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.0',
+    ruby_version => '*',
+  }
+}
